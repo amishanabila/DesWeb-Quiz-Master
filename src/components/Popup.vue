cdecc9f7042d6726a7e508df10daa7329c2d9a67
Amisha Nabila Wiguna <11231008@student.itk.ac.id>
2025-12-10 18:32:49 +0800

    last

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -4,7 +4,18 @@
       v-if="showNamePopup"
       class="fixed inset-0 bg-white/50 flex justify-center items-center z-50"
     >
-      <div class="bg-white p-8 rounded shadow text-center w-[90%] max-w-md">
+      <div class="bg-white p-8 rounded shadow text-center w-[90%] max-w-md relative">
+        <!-- Button X di pojok kanan -->
+        <button
+          @click="cancelQuiz"
+          class="absolute right-4 top-4 w-8 h-8 flex items-center justify-center rounded-full bg-red-500 hover:bg-red-600 text-white transition duration-200 shadow-md"
+          title="Batalkan"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+          </svg>
+        </button>
+
         <h2 class="text-2xl font-bold mb-4">Masukkan Nama Anda</h2>
 
         <input
